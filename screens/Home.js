5457d97ba88cbe80abf0b89356ec091eb08caf4c
otutsukihyuuga <111prathamshah@gmail.com>
2021-10-22 18:51:55 +0530

    api

--- screens/Home.js
+++ screens/Home.js
@@ -1,9 +1,22 @@
 import React from "react";
 import { View, Text } from "react-native";
 
+import { connect } from "react-redux";
+// import { holdings } from "../constants/dummy";
+import { getHoldings, getCoinMarket } from "../stores/market/marketActions";
+import { useFocusEffect } from "@react-navigation/native";
 import MainLayout from "./MainLayout";
+import { SIZES, COLORS, FONTS, dummyData, icons } from "../constants";
 
-const Home = () => {
+const Home = ({ getCoinMarket, getHoldings, myHoldings, coins }) => {
+
+	useFocusEffect(
+		React.useCallback(() => {
+			getHoldings(holdings = dummyData.holdings)
+			getCoinMarket()
+		}, [])
+	)
+
 	return (
 		<MainLayout>
 			<View>
@@ -13,4 +26,22 @@ const Home = () => {
 	);
 };
 
-export default Home;
+function mapStateToProps(state) {
+	return {
+		myHoldings: state.marketReducer.myHoldings,
+		coins: state.marketReducer.coins,
+	}
+}
+
+function mapDispatchToProps(dispatch) {
+	return {
+		getHoldings: (holdings, currency, coinList, orderBy, sparkline, priceChangePerc, perPage, page) => {
+			return dispatch(getHoldings(holdings, currency, coinList, orderBy, sparkline, priceChangePerc, perPage, page))
+		},
+		getCoinMarket: (currency, coinList, orderBy, sparkline, priceChangePerc, perPage, page) => {
+			return dispatch(getCoinMarket(currency, coinList, orderBy, sparkline, priceChangePerc, perPage, page))
+		}
+	}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
